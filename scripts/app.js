b3f25519b54d0c7fd96e421bfc1c7400c4310cea
thamudi <tamim.hamoudi@gmail.com>
2020-11-21 01:18:18 +0200

    main layout and styles added

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,32 @@
 'use strict'
+
+function checkNavItem(event) {
+    const navIdList = ['main-nav', 'work-nav', 'about-nav', 'contact-nav']
+    const navId = event.target.id;
+    
+    if (navIdList.includes(navId)) {
+        renderSection(navId);
+    }
+}
+
+/** 
+ *  --------------------
+ *  - Render Functions -
+ *  --------------------
+*/
+
+function renderSection(navId) {
+    const targetId = navId.split('-')[0];
+    document.querySelectorAll('.section').forEach(element => {
+        element.classList.add('hide');
+    });
+    document.getElementById(targetId).classList.remove('hide');
+
+}
+
+/** 
+ *  -------------------
+ *  - Event listeners -
+ *  -------------------
+*/
+document.getElementById('nav').addEventListener('click', checkNavItem);
