fcc1969800c6cda40dc925be8d13688156458164
thamudi <tamim.hamoudi@gmail.com>
2021-03-02 13:30:41 +0200

    emails enabled

--- scripts/app.js
+++ scripts/app.js
@@ -1,81 +1,120 @@
-'use strict'
+"use strict";
+/**
+ *  -----------------------------
+ *  - Global Variables -
+ *  -----------------------------
+ */
 
-/** 
+// const END_POINT = "http://127.0.0.1:8080/send-mail";
+const END_POINT = "https://express-mailer-thamudi.herokuapp.com/send-mail";
+
+class MailBody {
+  constructor(fname, lname, email, subj, msg) {
+    this.firstName = fname;
+    this.lastName = lname;
+    this.email = email;
+    this.subject = subj;
+    this.message = msg;
+    this.stringifiedData = this.stringifyData();
+  }
+
+  stringifyData() {
+    return JSON.stringify(this);
+  }
+}
+
+/**
  *  -----------------------------
  *  - Event Listeners Functions -
  *  -----------------------------
-*/
+ */
 
-function sendMail(event) { // send mail using fetch API
-    event.preventDefault();
+function sendMail(event) {
+  // send mail using fetch API
+  event.preventDefault();
 
-    const status = true;
-    
+  const mail = new MailBody(
+    event.target.fname.value,
+    event.target.lname.value,
+    event.target.email.value,
+    event.target.subj.value,
+    event.target.msg.value
+  );
 
-    if (status) { // check status response from the promise 
-        renderMessage('modal-success');
-    } else {
-        renderMessage('modal-fail')
-    }
+  (async () => {
+    const rawResponse = await fetch(END_POINT, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: mail.stringifiedData,
+    });
+    const content = await rawResponse.json();
 
-
-
+    content.status
+      ? renderMessage("modal-success")
+      : renderMessage("modal-fail");
+  })();
 }
 
-/** 
+/**
  *  --------------------
  *  - Render Functions -
  *  --------------------
-*/
-function toggleInfo(event) { // toggles slides info to show
-    const paths = event.path; // get the paths of all the possible elements within the clicked event
+ */
+function toggleInfo(event) {
+  // toggles slides info to show
+  const paths = event.path; // get the paths of all the possible elements within the clicked event
 
-    paths.forEach(path => {
+  paths.forEach((path) => {
+    if (path.className) {
+      // check if the current path has a class
 
-        if (path.className) { // check if the current path has a class
+      if (path.className.includes("info")) {
+        path.classList.toggle("active");
+      }
 
-            if (path.className.includes('info')) {
-                path.classList.toggle('active');
-            }
+      if (
+        path.className.includes("active") &&
+        path.className.includes("slide")
+      ) {
+        // check if its an active slide and add an overlay
+        path.classList.toggle("overlay"); // toggle on/ off the overlay
+      }
 
-            if (path.className.includes('active') && path.className.includes('slide')) { // check if its an active slide and add an overlay
-                path.classList.toggle('overlay'); // toggle on/ off the overlay 
-            }
+      if (path.className.includes("intro")) {
+        // check if its the parent element and show the text
 
-            if (path.className.includes('intro')) { // check if its the parent element and show the text
+        // not working ??
+        // path.children.forEach(child => {
+        //     console.log(child);
+        // });
 
-                // not working ??
-                // path.children.forEach(child => {
-                //     console.log(child);
-                // });
-
-                const child = path.children[1];
-                child.classList.toggle('z-in-show'); // toggle on/ off the z-index of the text
-            }
-        }
-
-    });
-
+        const child = path.children[1];
+        child.classList.toggle("z-in-show"); // toggle on/ off the z-index of the text
+      }
+    }
+  });
 }
 
 function renderMessage(modalId) {
-    
-    let count = 0;
+  let count = 0;
 
-    const loop = setInterval(() => {
-        if(count<=1){
-            document.getElementById(modalId).classList.toggle('opa-show'); 
-            count++;
-        } else {
-            clearInterval(loop)
-        }
-    }, 2000);
+  const loop = setInterval(() => {
+    if (count <= 1) {
+      document.getElementById(modalId).classList.toggle("opa-show");
+      count++;
+    } else {
+      clearInterval(loop);
+    }
+  }, 2000);
 }
 
-/** 
+/**
  *  -------------------
  *  - Event listeners -
  *  -------------------
-*/
+ */
 
-document.getElementById('contact-me').addEventListener('submit', sendMail);
+document.getElementById("contact-me").addEventListener("submit", sendMail);
